578671a61f8336eabab20cdf02c966e9af84812a
KevinBermudezC <kevinbermudezc@gmail.com>
2024-07-12 10:57:07 -0500

    Fixed: html and js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('Holi')
+const btnEncriptar = document.querySelector('#btn-encrip').value;
+btnEncriptar.addEv
+
+function encriptar(){
+    const info = btnEncriptar;
+    console.log(info)
+}
+encriptar()
